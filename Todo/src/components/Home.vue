62edb9b1352f81987fe9ca661393d7ab16e15703
kinyarasam <samkinyara2000@gmail.com>
2022-05-26 21:37:09 +0300

    using a proxy and json-server

--- Todo/src/components/Home.vue
+++ Todo/src/components/Home.vue
@@ -4,10 +4,10 @@
         <div class="mx-8 my-4 p-4 flex justify-center bg-[#2a2a2a] rounded shadow-lg shadow-[#6a6a6a]">
             <div>
                 <div>
-                    <AddTask />
+                    <AddTask @add_task="Add_Task" />
                 </div>
                 <div>
-                    SHOW TASKS
+                    <ShowTasks :tasks="tasks"/>
                 </div>
             </div>
         </div>
@@ -16,11 +16,29 @@
 
 <script>
 import AddTask from './AddTask.vue'
+import ShowTasks from './ShowTask.vue'
 
 export default {
     name: 'Home',
     components: {
         AddTask
+    },
+    data() {
+        return {}
+    },
+    methods: {
+        async addTask () {
+            const res = await fetch('api/tasks', {
+                method: 'POST',
+                header: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(task)
+            })
+            const data = await res.json()
+
+            this.tasks = [...this.tasks, data]
+        }
     }
 }
 </script>
---
+++ Todo/src/components/ShowTask.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-for="task in tasks" :key="task.id">
+            <Task :task="task"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+    name: 'ShowTasks',
+    components: {
+        Task
+    }
+
+}
+</script>
+
+<style>
+
+</style>

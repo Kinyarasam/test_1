6f19cf954de4d54768366585958757e6dcde2000
kinyarasam <samkinyara2000@gmail.com>
2022-05-26 20:47:46 +0300

    Implementing v-models on form

---
+++ Todo/src/components/AddTask.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="border-b-2 pb-3">
+        <form @submit.prevent="onSubmit">
+            <div>
+                <label for="task">Add Task:</label>
+                <input 
+                    v-model="task"
+                    name="Task" 
+                    type="text"
+                    placeholder="Add New Task"
+                    class="block bg-black px-2 rounded-lg py-1.5 focus:border-none"
+                />
+            </div>
+            <div class="mt-2">
+                <label for="task">Day & Time</label>
+                <input 
+                    name="day" 
+                    v-model="day"
+                    type="text"
+                    placeholder="Add Day and Time"
+                    class="block bg-black px-2 rounded-lg py-1.5 focus:border-none"
+                />
+            </div>
+            <div class="my-2">
+                <label for="task">Add Description:</label>
+                <input 
+                    name="Task"
+                    v-model="description"
+                    type="text"
+                    placeholder="Add New Task"
+                    class="block bg-black px-2 rounded-lg py-1.5 focus:border-none"
+                />
+            </div>
+            <div >
+                <label for="task">completed: </label>
+                <input 
+                    name="completed" 
+                    v-model="completed"
+                    type="checkbox"
+                    class="bg-black px-2 rounded-lg py-1.5 focus:border-none"
+                />
+            </div>
+            <input type="submit" 
+                class="bg-teal-500 px-5 py-1.5 rounded text-black font-semibold hover:scale-110"
+            />
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AddTask',
+    data() {
+        return {
+            task: '',
+            day: '',
+            description: '',
+            completed: false
+        }
+    },
+    methods: {
+        onSubmit(data) {
+            data.preventDefault()
+
+            const newTask = {
+                task: this.task,
+                day: this.day,
+                description: this.description,
+                completed: this.completed
+            }
+
+            this.$emit('add_Task', newTask)
+
+            this.task = '',
+            this.day = '',
+            this.description = ''
+            console.log(newTask)
+        }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
---
+++ Todo/src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bg-black text-[#d4d4d4] h-screen">
+        Home View
+        <div class="mx-8 my-4 p-4 flex justify-center bg-[#2a2a2a] rounded shadow-lg shadow-[#6a6a6a]">
+            <div>
+                <div>
+                    <AddTask />
+                </div>
+                <div>
+                    SHOW TASKS
+                </div>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+import AddTask from './AddTask.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        AddTask
+    }
+}
+</script>
